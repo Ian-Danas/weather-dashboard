2ca3fa00e2b7122fbbd711492af9895d6dfe212c
Ian Danas <ianmdanas@gmail.com>
2023-04-09 14:27:24 -0700

    adding in logic to save previous searchs and handler to be able to click on them to re-search

--- script.js
+++ script.js
@@ -4,22 +4,41 @@ var GeoURL = 'http://api.openweathermap.org/geo/1.0/direct?'
 var apiKey = 'appid=5eb1683a4381f8f5d69e894d1120f7a0'
 var forcastCont = $('.weather')
 var prevSearch = $('.prev-search')
+
 // forcastCont.hide()
 // prevSearch.hide()
 var searches = []
-// updateSearch()
+updateSearch()
+var savedBtn = $('.saved')
+savedBtn.on('click',function(){
+    console.log('clicked on a previous search')
+    var city = ($(this).text())
+    getApi(city)
+    
+  })
+
+
 
 searchBtn.on('click',function(){
     var searchVal = $('#search').val()
     if (searchVal != ''){
-        coords = getApi(searchVal)
+        getApi(searchVal)
         lastSearch = JSON.parse(localStorage.getItem('history'))||[]
-        if(lastSearch.length = 5){
-            lastSearch.pop()
+        if(lastSearch.includes(searchVal) == false){
+
             lastSearch.unshift(searchVal)
             localStorage.setItem('history',JSON.stringify(lastSearch))
+            updateSearch()
+            var savedBtn = $('.saved')
+            savedBtn.on('click',function(){
+                console.log('clicked on a previous search')
+                var city = ($(this).text())
+                getApi(city)
+                
+              })
         }
-        updateSearch()
+        
+        
     }
 
   })
@@ -39,21 +58,25 @@ searchBtn.on('click',function(){
       })
       .then(function(data){
           forcastCont.show()
+          console.log(data)
           var weatherArry = data.list
           var todayEl = $('#today')
           var todayH2 = todayEl.children('#cityTime')
           var todayTempLi = todayEl.children('#dataList').children('#temp')
           var todayWindLi = todayEl.children('#dataList').children('#wind')
           var todayHumLi = todayEl.children('#dataList').children('#hum')
+          var todayIconImg = todayEl.children('.icon')
           var todayTemp = weatherArry[0].main.temp
           var todayWind = weatherArry[0].wind.speed
           var todayHum = weatherArry[0].main.humidity
+          var todayIcon = weatherArry[0].weather[0].icon
           var todayDate = dayjs(weatherArry[0].dt_txt).format('M/D/YYYY')
           var cityName = data.city.name
           todayTempLi.text('Temp: ' + todayTemp + ' F' + '\u00B0') 
           todayWindLi.text('Wind: ' + todayWind + ' MPH')
           todayHumLi.text('Humidity: ' + todayHum + '%')    
           todayH2.text(cityName + "(" + todayDate + ")")
+          todayIconImg.attr('src','https://openweathermap.org/img/wn/'+todayIcon+'@2x.png')
           
           var dayCounter = 1
      
@@ -63,14 +86,17 @@ searchBtn.on('click',function(){
             var dayTempLi = dayEl.children('#dataList').children('#temp')
             var dayWindLi = dayEl.children('#dataList').children('#wind')
             var dayHumLi = dayEl.children('#dataList').children('#hum')
+            var dayIconImg = dayEl.children('.icon')
             var dayTemp = weatherArry[i].main.temp
             var dayWind = weatherArry[i].wind.speed
             var dayHum = weatherArry[i].main.humidity
+            var dayIcon = weatherArry[i].weather[0].icon
             var dayDate = dayjs(weatherArry[i].dt_txt).format('M/D/YYYY')
             dayTempLi.text('Temp: ' + dayTemp + ' F' + '\u00B0') 
             dayWindLi.text('Wind: ' + dayWind + ' MPH')
             dayHumLi.text('Humidity: ' + dayHum + '%')  
             dayH2.text("(" + dayDate + ")")
+            dayIconImg.attr('src','https://openweathermap.org/img/wn/'+dayIcon+'@2x.png')
             dayCounter++
             
            }
@@ -81,11 +107,13 @@ searchBtn.on('click',function(){
 
 function updateSearch(){
     searchArry = JSON.parse(localStorage.getItem('history'))||[]
+    historyUl = $('.prev-search')
+    historyUl.empty()
     for (let i = 0; i < searchArry.length; i++) {
-        var child = '.prev-search:nth-child(' + i + ')'
-        var searchLi = $(child)
-        city = searchArry[i]
-        searchLi.text(city)
+        listEl ='<button class = saved>'+searchArry[i]+'</button>'
+        historyUl.append(listEl)
+    
         
     }
 }
+
